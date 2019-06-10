39126bee53b550dba1f61b6a2b844331cd953877
George <2392568162@qq.com>
2019-06-10 15:53:11 +0800

    new

---
+++ 62.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquepaths(self, m, n):
+        if m <= 0 or n <= 0:
+            return 0
+        if m == 1 or n ==1:
+            return 1
+        dp = [[1]*m for i in range(n)]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+    
+
